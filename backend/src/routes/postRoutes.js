7d5e46e5af5c7c1f3813c2bca15006eae38d266e
Padmavathi SJ <149775301+Padmavathi-SJ@users.noreply.github.com>
2025-03-20 13:07:12 +0530

    Update postRoutes.js

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -3,10 +3,10 @@ import { getMaxCommentedPosts, getLatestPosts } from "../Controllers/postControl
 
 const router = express.Router();
 
-// Route for fetching posts with the most comments
+
 router.get("/top-commented", getMaxCommentedPosts);
 
-// Route for fetching latest posts in real-time
+
 router.get("/latest", getLatestPosts);
 
 export default router;
